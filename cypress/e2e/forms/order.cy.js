b584f3a62ca992b3eb2f038a7ebd7552eed931f7
Martin Klemsa <m.klemsa@gmail.com>
2024-04-11 14:26:49 +0200

    Added test for order form

--- cypress/e2e/forms/order.cy.js
+++ cypress/e2e/forms/order.cy.js
@@ -12,4 +12,25 @@
 		cy.changeLanguage('Česky');
 		cy.contains('Objednat Skolaris');
 	});
+
+	it.only('Fills out the form', () => {
+		cy.get('material-textfield[params*="textInput: school"]').click().find('input').type('Test school');
+		cy.get('material-select[params*="value: country"]:visible').click();
+		cy.get('.mdc-menu-surface--open').contains('Czech Republic').click();
+		cy.get('material-button[params*="click: getRecommendation"]').click();
+		cy.get('.mdc-card:visible');
+		cy.get('material-checkbox[params*="checked: form.consent"]').scrollIntoView().click();
+		cy.get('material-button:contains("Continue"):visible').click();
+
+		cy.contains('Choose your payment method').should('be.visible');
+		cy.get('material-button:contains("Continue without payment"):visible').click();
+
+		cy.get('material-textfield[params*="textInput: firstName"]').click().find('input').type('Martin');
+		cy.get('material-textfield[params*="textInput: lastName"]').click().find('input').type('Klemsa');
+		cy.get('material-textfield[params*="textInput: address1"]').click().find('input').type('Sochorova 21');
+		cy.get('material-textfield[params*="textInput: zip"]').click().find('input').type('61600');
+		cy.get('material-textfield[params*="textInput: taxNumber"]').click().find('input').type('0123456789');
+		cy.get('material-textfield[params*="textInput: email"]').click().find('input').type('martin.klemsa@skolaris.net');
+		cy.get('material-button:contains("Place order"):visible').find('button').should('be.enabled');
+	});
 });
--- cypress/e2e/start.cy.js
+++ cypress/e2e/start.cy.js
@@ -14,4 +14,32 @@
 		cy.changeLanguage('Česky');
 		cy.contains('Tvorba rozvrhů');
 	});
+
+	it('Opens the quality case study', () => {
+		cy.get('section[data-pagescroll-label="power"]')
+			.find('material-button')
+			.contains('Learn more')
+			.click();
+
+		cy.contains('Improving the quality of education').should('be.visible');
+	});
+
+	it('Opens the budget case study', () => {
+		cy.get('section[data-pagescroll-label="value"]')
+			.find('material-button')
+			.contains('Learn more')
+			.click();
+
+		cy.contains('Keeping your budget').should('be.visible');
+	});
+
+	it('Navigates to order page', () => {
+		cy.get('material-button:contains("Order now"):visible').click();
+		cy.contains('Order Skolaris').should('be.visible');
+	});
+
+	it('Navigates to registration page', () => {
+		cy.get('material-button:contains("Try free"):visible').click();
+		cy.contains('Try Skolaris free').should('be.visible');
+	});
 });
